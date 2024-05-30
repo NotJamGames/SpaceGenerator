58c7e2b63110cc0d6a926ee4c2cdd5bb71f20145
= <harrisonkaye93@gmail.com>
2024-05-30 22:21:55 +0100

    Nebula generation and dithering

--- SpaceGenerator/space_generator.gd
+++ SpaceGenerator/space_generator.gd
@@ -3,3 +3,20 @@ extends Node
 
 @export var subviewport : SubViewport
 @export var star_generator : StarGenerator
+
+@export var nebulae : Array[Sprite2D]
+
+
+func _ready() -> void:
+	generate_space(Vector2i(360, 240))
+
+
+func generate_space(size : Vector2i) -> void:
+	subviewport.size = size
+
+	for nebula in nebulae:
+		nebula.texture.noise.seed = randi()
+		nebula.texture.width = size.x
+		nebula.texture.height = size.y
+
+	star_generator.generate_stars(97, [65.0, 20.0, 15.0], size)
