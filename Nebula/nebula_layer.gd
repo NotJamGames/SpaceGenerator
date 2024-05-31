92b536df29924f63b041f7eafaba1768b011ade6
= <harrisonkaye93@gmail.com>
2024-06-01 00:57:25 +0100

    Nebula Layers as isolated scene
    Made nebula layers independent so can be added to generated scene modularly

---
+++ Nebula/nebula_layer.gd
@@ -0,0 +1,86 @@
+@tool
+class_name NebulaLayer
+extends TextureRect
+
+
+@export_category("Node Paths")
+@export var subviewport : SubViewport
+@export var sprite : Sprite2D
+
+
+const nebula_dither_shader : Shader = preload\
+		("res://Nebula/nebula_dither_shader.gdshader")
+
+
+@export_category("Nebula Parameters")
+@export var palette : Texture : set = set_palette
+@export var contrast : float = 1.0 : set = set_contrast
+@export var threshold : float = 1.0 : set = set_threshold
+@export var alpha : float = 1.0 : set = set_alpha
+
+
+@onready var noise_texture : NoiseTexture2D = NoiseTexture2D.new()
+@onready var shader_material : ShaderMaterial = ShaderMaterial.new()
+
+
+func _ready() -> void:
+	shader_material.shader = nebula_dither_shader
+	sprite.material = shader_material
+	set_contrast(contrast)
+	set_threshold(threshold)
+	set_alpha(alpha)
+
+
+func build_nebula(new_base_size : Vector2i) -> void:
+	size = Vector2(size.x, size.y * 2)
+	subviewport.size = new_base_size
+
+	noise_texture.seamless = true
+	noise_texture.width = new_base_size.x
+	noise_texture.height = new_base_size.y
+
+	set_palette(palette)
+
+	var noise : FastNoiseLite = FastNoiseLite.new()
+	noise.seed = randi()
+	noise_texture.noise = noise
+
+	sprite.texture = noise_texture
+	sprite.centered = false
+
+
+func set_palette(new_palette : Texture) -> void:
+	palette = new_palette
+	if noise_texture == null or shader_material == null: return
+
+	var new_gradient : Gradient = Gradient.new()
+	new_gradient.add_point(.0, Color(.0, .0, .0, .0))
+
+	var palette_width : int = palette.get_width()
+	var palette_image : Image = palette.get_image()
+	for i : int in palette_width:
+		new_gradient.add_point\
+				(.36 + ((i + .5) * (.64 / palette_width)),
+				palette_image.get_pixel(i, 0))
+
+	noise_texture.color_ramp = new_gradient
+
+	shader_material.set_shader_parameter("palette", palette)
+
+
+func set_contrast(new_contrast : float) -> void:
+	contrast = new_contrast
+	if shader_material == null: return
+	shader_material.set_shader_parameter("contrast", contrast)
+
+
+func set_threshold(new_threshold : float) -> void:
+	threshold = new_threshold
+	if shader_material == null: return
+	shader_material.set_shader_parameter("threshold", threshold)
+
+
+func set_alpha(new_alpha) -> void:
+	alpha = new_alpha
+	if shader_material == null: return
+	shader_material.set_shader_parameter("alpha", alpha)
--- SpaceGenerator/space_generator.gd
+++ SpaceGenerator/space_generator.gd
@@ -4,7 +4,8 @@ extends Node
 @export var subviewport : SubViewport
 @export var star_generator : StarGenerator
 
-@export var nebulae : Array[Sprite2D]
+@export var old_nebulae : Array[Sprite2D]
+@export var nebula_layers : Array[NebulaLayer]
 
 
 func _ready() -> void:
@@ -14,9 +15,12 @@ func _ready() -> void:
 func generate_space(size : Vector2i) -> void:
 	subviewport.size = size
 
-	for nebula in nebulae:
+	for nebula in old_nebulae:
 		nebula.texture.noise.seed = randi()
 		nebula.texture.width = size.x
 		nebula.texture.height = size.y
+
+	for layer : NebulaLayer in nebula_layers:
+		layer.build_nebula(Vector2i(360, 240))
 
 	star_generator.generate_stars(97, [65.0, 20.0, 15.0], size)
