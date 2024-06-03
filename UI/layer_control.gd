623648d93375eef786489c91a126b8848f33d0d4
= <harrisonkaye93@gmail.com>
2024-06-03 22:12:15 +0100

    Delete layers

--- UI/layer_control.gd
+++ UI/layer_control.gd
@@ -1,8 +1,16 @@
 extends HBoxContainer
 
 
+@export var line_edit : LineEdit
 @export var matched_layer : GeneratorLayer
+
+
+signal request_deletion()
 
 
 func toggle_visible(new_state : bool) -> void:
 	matched_layer.visible = !new_state
+
+
+func delete() -> void:
+	request_deletion.emit(line_edit.text, [matched_layer, self])
--- UI/ui_manager.gd
+++ UI/ui_manager.gd
@@ -1,10 +1,17 @@
-extends HBoxContainer
+extends Control
 
 
 @export var left_panel : Array[PanelContainer]
 
+@export var delete_layer_dialogue : ColorRect
+@export var delete_layer_dialogue_label : Label
+
+
+var queued_deletions : Array = []
+
 
 func toggle_panel(panel_ref : String) -> void:
+	print("h")
 	var panels : Array[PanelContainer] = get(panel_ref)
 	if panels == null:
 		push_error("Error: panel %s not found" % panel_ref)
@@ -12,3 +19,25 @@ func toggle_panel(panel_ref : String) -> void:
 
 	for panel : PanelContainer in panels:
 		panel.visible = !panel.visible
+
+
+func create_layer() -> void:
+	pass
+	# remember to connect layer's signals here
+
+
+func query_delete_layer(layer_name : String, to_delete : Array) -> void:
+	delete_layer_dialogue_label.text = "Delete Layer: %s?" % layer_name
+	delete_layer_dialogue.visible = true
+	queued_deletions = to_delete
+
+
+func confirm_delete_layer() -> void:
+	for node : Node in queued_deletions:
+		node.queue_free()
+
+	delete_layer_dialogue.visible = false
+
+
+func cancel_delete_layer() -> void:
+	delete_layer_dialogue.visible = false
