f0d5b7a06590cd0be001920bd4b074d9fd65ea52
= <harrisonkaye93@gmail.com>
2024-06-07 02:02:52 +0100

    Adjustable nebula parameters

--- UI/nebula_layer_control_panel.gd
+++ UI/nebula_layer_control_panel.gd
@@ -1,6 +1,20 @@
 extends LayerControlPanel
 
 
+# TODO: figure out how to implement palettes
+# and fix this
+@export var palette_button : Control
+
+@export var threshold_slider : HSlider
+@export var alpha_slider : HSlider
+
+@export var oscillation_checkbox : CheckBox
+@export var oscillation_intensity_slider : HSlider
+@export var oscillation_rate_slider : HSlider
+@export var oscillation_offset_slider : HSlider
+@export var speed_slider : HSlider
+
+
 var layer : GeneratorLayer
 
 var closed : bool = true
@@ -14,6 +28,16 @@ func configure_and_open\
 	if layer == null:
 		push_error("Error: layer not of type NebulaLayer")
 
+	threshold_slider.value = layer.threshold
+	alpha_slider.value = layer.alpha
+
+	oscillation_checkbox.button_pressed = layer.oscillate
+	oscillation_intensity_slider.value = layer.oscillation_intensity
+	oscillation_rate_slider.value = layer.oscillation_rate
+	oscillation_offset_slider.value = layer.oscillation_rate
+	speed_slider.value = layer.speed
+	
+
 	closed = false
 	visible = true
 
@@ -21,3 +45,31 @@ func configure_and_open\
 func close() -> void:
 	visible = false
 	closed = true
+
+
+func update_threshold(new_value : float) -> void:
+	layer.threshold = new_value
+
+
+func update_alpha(new_value : float) -> void:
+	layer.alpha = new_value
+
+
+func update_oscillate(new_state : bool) -> void:
+	layer.oscillate = new_state
+
+
+func update_oscillation_intensity(new_value : float) -> void:
+	layer.oscillation_intensity = new_value
+
+
+func update_oscillation_rate(new_value : float) -> void:
+	layer.oscillation_rate = new_value
+
+
+func update_oscillation_offset(new_value : float) -> void:
+	layer.oscillation_offset = new_value
+
+
+func update_speed(new_value : float) -> void:
+	layer.speed = new_value
