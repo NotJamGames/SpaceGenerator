8211f95e472ae73433f6f13bdb6690e452944b67
= <harrisonkaye93@gmail.com>
2024-06-21 21:40:17 +0100

    Rename h_range_slider, planet scroll speed slider

--- UI/planet_layer_control_panel.gd
+++ UI/planet_layer_control_panel.gd
@@ -1,6 +1,13 @@
 extends LayerControlPanel
 
 
+@export var spawn_interval_hrange_slider : HRangeSlider
+@export var max_concurrent_planets_spin_box : SpinBox
+
+
+@export var scroll_speed_slider : HSlider
+
+
 var layer : PlanetLayer
 
 
@@ -12,8 +19,19 @@ func configure_and_open\
 	if layer == null:
 		push_error("Error: layer not of type PlanetLayer")
 
+	scroll_speed_slider.value = layer.speed
+
 	closed = false
 	visible = true
+
+
+func update_spawn_interval(new_min : float, new_max : float) -> void:
+	layer.min_spawn_frequency = new_min
+	layer.max_spawn_frequency = new_max
+
+
+func update_max_concurrent_planets(new_value : int) -> void:
+	layer.max_concurrent_planets = new_value
 
 
 func update_speed(value : float) -> void:
